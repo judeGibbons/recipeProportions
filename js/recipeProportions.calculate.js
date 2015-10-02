319997f3ff209399876516be8a137a91f01d4d2d
judeGibbons <judes@gimaju.net>
2015-10-02 14:01:07 +0100

    Produces correct length canvas bars

--- js/recipeProportions.calculate.js
+++ js/recipeProportions.calculate.js
@@ -2,102 +2,83 @@ var recipeProportionsModule = recipeProportionsModule || {};
 
 //recipeFinderModule.loadRecipes = function () {
 
-  document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-    if (document.getElementById("saveIngredientButton").addEventListener) {
-      (document.getElementById("saveIngredientButton")).addEventListener('click', submitParameters, false);
-    } else {
-    if (document.getElementById("saveIngredientButton").attachEvent) {
-      (document.getElementById("saveIngredientButton")).attachEvent('onclick', submitParameters);
-      };
+  if (document.getElementById("saveIngredientButton").addEventListener) {
+    (document.getElementById("saveIngredientButton")).addEventListener('click', submitParameters, false);
+  } else {
+  if (document.getElementById("saveIngredientButton").attachEvent) {
+    (document.getElementById("saveIngredientButton")).attachEvent('onclick', submitParameters);
     };
+  };
 
-  });
+  canvas = document.getElementById("recipeBar");
+    if (canvas.getContext) {
+    ctx = canvas.getContext('2d');
+  }; 
+
+});
 
 
-  var totalBarLength = 600; //needs to match width of canvas - use variable?
-  var totalAmount = 0;
-  var recipeProportions = [];
-  var canvas;
+var totalBarLength = 600; //needs to match width of canvas - use variable?
+var totalAmount = 0;
+var recipeProportions = [];
+var canvas, ctx;
 
-  function submitParameters(event) {
-  //need to check for nonsense values here
-    var ingredient = (document.getElementById("ingredient").value);
-    var count = (document.getElementById("count").value);
-    var units = (document.getElementById("units").value);
-    (event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+function submitParameters(event) {
+//need to check for nonsense values here - one stray char will break whole array
+//clear form on reload?
+  var ingredient = (document.getElementById("ingredient").value);
+  var count = (document.getElementById("count").value);
+  var units = (document.getElementById("units").value);
+  (event.preventDefault) ? event.preventDefault() : event.returnValue = false;
 
-    normaliseAmount(ingredient,count,units);
-  }
+  normaliseAmount(ingredient,count,units);
+};
 
 //runs each time ingredient entered
-  function normaliseAmount(ingredient,count,units) {
+function normaliseAmount(ingredient,count,units) {
+  var normalisedAmount = count; //do something with units here
+  createObject(ingredient,normalisedAmount,count,units);
+};
 
-    var normalisedAmount = count; //do something with units here
-    var ingredientObject = {};
+function createObject(ingredient,normalisedAmount,count,units) {
 
-    ingredientObject.ingredient = ingredient;
-    ingredientObject.normalisedAmount = +normalisedAmount;
-    ingredientObject.count = +count;
-    ingredientObject.units = units;
+  var ingredientObject = {};
 
-    recipeProportions.push(ingredientObject);
+  ingredientObject.ingredient = ingredient;
+  ingredientObject.normalisedAmount = +normalisedAmount;
+  ingredientObject.count = +count;
+  ingredientObject.units = units;
 
-    totalAmount += ingredientObject.normalisedAmount; //increases every time, maybe move?
+  recipeProportions.push(ingredientObject);
 
-    drawBar(ingredientObject,totalAmount);
-  };
+  totalAmount += ingredientObject.normalisedAmount;
 
-  //this is recalculated every time, 
-  function drawBar(ingredientObject,totalAmount) {
-    for (i=0, maxi=recipeProportions.length; i<maxi; i++) {
-      var barLength;
-      recipeProportions[i].barLength = Math.round((recipeProportions[i].normalisedAmount/totalAmount) * totalBarLength);
-    
-      console.log(recipeProportions);
-  
-      canvas = document.getElementById("recipeBar");
-      if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');  
-        ctx.clearRect(0, 0, 600, 20);  //do i need to redeclare these each time?
-        if (i==0) {
-          ctx.fillStyle = "#00ff00";
-        } else if (i==1) {
-          ctx.fillStyle = "#ff0000";
-        } else if (i==2) {
-          ctx.fillStyle = "#0000ff";
-        } else if (i==3) {
-          ctx.fillStyle = "#ff00ff";
-        }; 
-        /*
-        if (i==1) {
-          ctx.fillStyle = "#00ff00";
-        } else {
-          ctx.fillStyle = "transparent";
-        };
+  drawBar(recipeProportions,totalAmount);
+};
 
+function drawBar(recipeProportions,totalAmount) {
 
-        if (i%2==0) {
-          ctx.fillStyle = "#00ff00";
-        } else {
-          ctx.fillStyle = "#ff0000";
-        };*/
-        
-        ctx.fillRect(0,0,recipeProportions[i].barLength,20);
-        //first 0 has to change to ...[i-1].barLength
-        //needs to redraw
-        console.log(recipeProportions[i].barLength);
-      }
+  ctx.clearRect(0, 0, 600, 20);
+  var barLength, barOffset=0;
+
+  for (i=0, maxi=recipeProportions.length; i<maxi; i++) {
+
+    recipeProportions[i].barLength = Math.round((recipeProportions[i].normalisedAmount/totalAmount) * totalBarLength);
+
+      if (i==0) {
+        ctx.fillStyle = "#00ff00";
+      } else if (i==1) {
+        ctx.fillStyle = "#ff0000";
+      } else if (i==2) {
+        ctx.fillStyle = "#0000ff";
+      } else if (i==3) {
+        ctx.fillStyle = "#ff00ff";
+      }; 
+
+      ctx.fillRect(barOffset,0,recipeProportions[i].barLength,20);
+
+      barOffset += recipeProportions[i].barLength;
     }
-  };
-
-
-
-/*
-currently, 100, 200, 300, 400
-produces a line with = 
-100
-200, 100
-300, 100, 200
-400, 100, 200, 300
-*/
+};
